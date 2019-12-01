39179692d834165dbf5b8b7cc7a8858fa43cbec8
박태규 <computerpark@postech.ac.kr>
2019-12-02 01:40:27 +0900

    organized

--- YUY sibal.py
+++ YUY sibal.py
@@ -5,32 +5,42 @@ import jetson_nano_move as jm
 import numpy as np
 
 
-##import modules
+# import modules
 
-def check_location(thr, height, width):
-    Rave = 0
-    Lave = 0
-    for i in range(height//2, height):
-        Ridx = int(width)
-        Lidx = int(0)
+
+def is_white(color_code):
+    return color_code == 255
+
+
+def white_dispersion_average(image_input, height_input, width_input):
+    if type(height_input) is not int or type(width_input) is not int:
+        raise TypeError('height_input and width_input must be an int!')
+    right_average = 0
+    left_average = 0
+    for i in range(height_input // 2, height_input):
+        right_dispersion = width_input // 2
+        left_dispersion = width_input // 2
         # range -> white check
-        for j in range(width // 2, 0, -1):
-            if thr[i][j] == 255:
-                Lidx = j
+        for j in range(width_input // 2, -1, -1):
+            if is_white(image_input[i][j]):
+                left_dispersion = width_input // 2 - j
                 break
-        for j in range(width // 2, width, 1):
-            if thr[i][j] == 255:
-                Ridx = j
+        for j in range(width_input // 2, width_input):
+            if is_white(image_input[i][j]):
+                right_dispersion = j - width_input // 2
                 break
-        Lidx = float(width // 2 - Lidx)
-        Ridx = float(Ridx - width // 2)
-        Rave += Ridx
-        Lave += Lidx
-    return Lave, Rave
+        right_average += right_dispersion
+        left_average += left_dispersion
+    return left_average, right_average
+
+
+def finish_program(video_capture):
+    video_capture.release()
+    cv.destroyAllWindows()
+
 
 # initial condition
 img = jm.cap
-# img = cv.VideoCapture(0)
 img.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
 img.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
 
@@ -56,18 +66,18 @@ while True:
 
     # setup values
     height, width = thr.shape
-    Lave, Rave = check_location(thr, height, width)
+    Lave, Rave = white_dispersion_average(thr, height, width)
 
     # case 1
     ratio = Rave / Lave
     ratio = m.log2(ratio)
-    if ratio>0:
+    if ratio > 0:
         print(ratio, min(120.0, 90.0 + ratio * 20.0), 0.2 - m.fabs(ratio / 100))
         jm.set_angle(min(120.0, 90.0 + ratio * 20.0))
     else:
         print(ratio, max(60.0, 90.0 + ratio * 20.0), 0.2 - m.fabs(ratio / 100))
         jm.set_angle(max(60.0, 90.0 + ratio * 20.0))
-    jm.set_throttle(0.3-m.fabs(ratio/100))
+    jm.set_throttle(0.3 - m.fabs(ratio / 100))
 
     # case 2
     # ratio = Rave - Lave
@@ -80,15 +90,11 @@ while True:
 
     # case 4
 
-
     # default : 양수 -> 오른쪽, 음수 -> 왼쪽, ratio = 0 -> 90도
-
 
 # img = cv.imread('sample1.jpg', cv.IMREAD_GRAYSCALE)
 # cv.imshow('xxx', img) // 담 작성 - 샘플 이미지 입출력 코드
 
 cv.waitKey(0)
 
-img.release()
-
-cv.destroyAllWindows()
+finish_program(img)
